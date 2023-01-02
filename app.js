fe4988763dcacd05c9e551620138e47650656a67
adomasr <adomas.razmus@gmail.com>
2023-01-02 22:44:01 +0000

    length and include numbers options

--- app.js
+++ app.js
@@ -1,37 +1,52 @@
 
 
 const formEl = document.getElementById('form-el');
+const passLengthEl = document.getElementById('length');
+const numCheckEl = document.getElementById('num-check');
 
 
 
+//max pass length - 24
 
 
-formEl.addEventListener('submit', function(event) {
-    getPass(event, 8);
-})
-
+formEl.addEventListener('submit', getPass);
 
 
 
-function getPass(event, passLength) {
+function getPass(event) {
     event.preventDefault();
     let password = '';
-    const abcArray = makeAbcData();
-     
-    for (let i=0; i<passLength; i++) {
-        const rNum = Math.floor(Math.random() * 26); //26 letters, interv 0 - 25
-        const rChar = abcArray[rNum];
+    let passwordLen = passLengthEl.value;
+    const data = makeData();
+    //making the 1 array of 2
+    if (numCheckEl.checked) {
+        passwordLen = passwordLen - 1;
+        for (let element of data.numbers) {
+            data.abc.push(element)
+    }}
+    // console.log(data);
+    for (let i=0; i<passwordLen; i++) {
+        const rNum = Math.floor(Math.random() * data.abc.length); 
+        const rChar = data.abc[rNum];
         password += rChar;
         //console.log(rChar);
     }
-    //console.log(password);
+    //adding a number to the end of the password
+    if (passLengthEl.value > 0 && numCheckEl.checked) {
+      password += data.numbers[Math.floor(Math.random() * data.numbers.length)]; 
+    }
+    // console.log(passLengthEl.value);
     formEl.children[1].value = password;
+    console.log();
 }
 
 
 
-function makeAbcData() {
+function makeData() {
     const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-    return Array.from(String(alphabet));
-   
+    const numbers = '0123456789'
+    return {
+        abc: Array.from(alphabet),
+        numbers: Array.from(numbers)
+    }
 }
